0570abe9b06373cdb330eec2ff8c59cde8f8eac1
ethan-clark-rams <141266028+ethan-clark-rams@users.noreply.github.com>
2023-08-07 14:14:48 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -- Project information -----------------------------------------------------
 
-project = 'Site Title'
-copyright = '2022, Your name'
-author = 'Your Name'
+project = 'Giggity'
+copyright = '2023, Ethan Clark'
+author = 'Ethan Clark'
 
 
 
